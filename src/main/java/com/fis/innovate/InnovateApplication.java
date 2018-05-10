dffc003d49474798cee4ab7564e3b7600794a899
Vivek Tandon <Vivek.Tandon@fisglobal.com>
2018-05-10 17:44:20 +0530

    Initial Commit

---
+++ src/main/java/com/fis/innovate/InnovateApplication.java
@@ -0,0 +1,22 @@
+package com.fis.innovate;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication (exclude = SecurityAutoConfiguration.class)
+public class InnovateApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(InnovateApplication.class, args);
+		String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames)
+        {
+            System.out.println(beanName);
+        }
+	}
+}
---
+++ src/main/java/com/fis/innovate/model/Signature.java
@@ -0,0 +1,5 @@
+package com.fis.innovate.model;
+
+public class Signature {
+
+}
